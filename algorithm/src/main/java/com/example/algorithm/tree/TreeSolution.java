f4e68c4e58d280b23e7700da1927ea61fc8879c8
hejinghui <jinghui.he@symbio.com>
2021-02-07 17:59:05 +0800

    add KotlinMain.kt

--- algorithm/src/main/java/com/example/algorithm/tree/TreeSolution.java
+++ algorithm/src/main/java/com/example/algorithm/tree/TreeSolution.java
@@ -14,4 +14,49 @@ public class TreeSolution {
         return Math.max(leftDepth, rightDepth) + 1;
     }
 
+    public static boolean isBalanced(TreeNode root) {
+        return isBST(root).isB;
+    }
+
+    static class ReturnNode {
+        boolean isB;
+        int depth;
+
+        //构造方法
+        public ReturnNode(int depth, boolean isB) {
+            this.depth = depth;
+            this.isB = isB;
+        }
+    }
+
+    //参考递归套路的第三部：描述单次执行过程是什么样的
+    //这里的单次执行过程具体如下：
+    //是否终止?->没终止的话，判断是否满足不平衡的三个条件->返回值
+    public static ReturnNode isBST(TreeNode root) {
+        if (root == null) {
+            return new ReturnNode(0, true);
+        }
+        //不平衡的情况有3种：左树不平衡、右树不平衡、左树和右树差的绝对值大于1
+        ReturnNode left = isBST(root.left);
+        ReturnNode right = isBST(root.right);
+        if (left.isB == false || right.isB == false) {
+            return new ReturnNode(0, false);
+        }
+        if (Math.abs(left.depth - right.depth) > 1) {
+            return new ReturnNode(0, false);
+        }
+        //不满足上面3种情况，说明平衡了，树的深度为左右俩子树最大深度+1
+        return new ReturnNode(Math.max(left.depth, right.depth) + 1, true);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 }
